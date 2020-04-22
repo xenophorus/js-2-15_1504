5800d117d8d54b768b5d6c6f4a1fcddaeaeb73e1
sharapov2701 <50914537+sharapov2701@users.noreply.github.com>
2020-04-22 23:28:37 +0300

    HW2

--- Students/sharapov2701/project/src/index.js
+++ Students/sharapov2701/project/src/index.js
@@ -1,1 +1,9 @@
+import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './layout/styles/style.css';
+import './layout/styles/normalize.css';
+
 import app from './js/main.js'
+
+app();
