b3160f956965ce153a1033f7e53d7b4a0123b85c
alkanaft <alkanaft123@yandex.ru>
2020-04-22 12:30:50 +0300

    lesson2
    l2

--- Students/GrigorevFedor/Project/src/index.js
+++ Students/GrigorevFedor/Project/src/index.js
@@ -1,3 +1,8 @@
-import myfunc from "./js/main"
+import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-myfunc();
+import './layout/styles/style.css';
+
+import app from './script/js/main.js';
+
+app();
