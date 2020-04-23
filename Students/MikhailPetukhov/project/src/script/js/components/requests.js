ead4331cd44185585a7bd8842af9554d4693553c
Mikhail Petukhov <petmik@yandex.ru>
2020-04-23 20:48:42 +0300

    requests added

---
+++ Students/MikhailPetukhov/project/src/script/js/components/requests.js
@@ -0,0 +1,32 @@
+function getData(reqUrl) {
+     return new Promise((res, rej) => {
+        let req = new XMLHttpRequest(); //если не считаем IE
+        req.open('GET', reqUrl, true); 
+                
+        req.onreadystatechange = function () {
+            if (req.readyState == 4) {
+                if(req.status == 200) {
+                    res(JSON.parse(req.responseText));
+                } else {
+                    rej('error');
+                }
+            }
+        };
+        req.send(); 
+    }) 
+}
+
+export default function exec(rUrl) {
+    getData(rUrl)
+        .then(data => {
+            // console.log(data);
+            return data
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        .finally(() => {
+            console.log('request end')
+        })
+
+}
--- Students/MikhailPetukhov/project/src/script/js/main_old.js
+++ Students/MikhailPetukhov/project/src/script/js/main_old.js
@@ -1,82 +1,65 @@
  //ИМИТАЦИЯ РАБОТЫ БАЗЫ ДАННЫХ И СЕРВЕРА
 
- // let PRODUCTS_NAMES = ['Комбинезон', 'Куртка', 'Куртка', 'Пальто', 'Куртка']
- // let PRICES = [2000, 1200, 1600, 1800, 1200]
- // let IDS = [0, 1, 2, 3, 4]
- // let IMGS = ['./assets/img/10-88-325-3.jpg', 
- // './assets/img/11-150-317-1.jpg',
- // './assets/img/102-854-143-6605-1.jpg',
- // './assets/img/108-506-143-4035-1.jpg',
- // './assets/img/12608-JOEL-611-848-1.jpg']
+ let PRODUCTS_NAMES = ['Комбинезон', 'Куртка', 'Куртка', 'Пальто', 'Куртка']
+ let PRICES = [2000, 1200, 1600, 1800, 1200]
+ let IDS = [0, 1, 2, 3, 4]
+ let IMGS = ['./assets/img/10-88-325-3.jpg', 
+ './assets/img/11-150-317-1.jpg',
+ './assets/img/102-854-143-6605-1.jpg',
+ './assets/img/108-506-143-4035-1.jpg',
+ './assets/img/12608-JOEL-611-848-1.jpg']
 
  //let products = [] //массив объектов
-
-
-import exec from "./components/requests.js";
-
- class Catalog {
-    constructor(cart) {
-        this.items = [];
-        this.container = '.products';
-        this.cart = cart;
-        this._init();
-    }
-
+ 
+ let catalog = {
+    items: [],
+    container: '.products',
+    cart: null,
+    construct (cart) {
+        this.cart = cart
+        this._init () //_ - это обозначение инкапсулированного метода
+    },
     _init () {
-        this._handleRequest()
-        // this._handleData ()
+        this._handleData ()
         this.render ()
         this._handleEvents ()
-
-    }
-
+    },
     _handleEvents () {
         document.querySelector (this.container).addEventListener ('click', (evt) => {
             if (evt.target.name === 'buy-btn') {
                 this.cart.addProduct (evt.target)
             }
         })
-    }
-
-
-    // _handleData () {
-    //     for (let i = 0; i < IDS.length; i++) {
-    //         this.items.push (this._createNewProduct (i))
-    //     }
-    // }
-
-    _handleRequest() {
-        console.log("request started");
-        let url = 'https://raw.githubusercontent.com/petmik2018/shop_data/master/products/products.json';
-        let result = exec(url);
-        console.log(result);
-    }
-
-    // _createNewProduct (index) {
-    //     return {
-    //         product_name: PRODUCTS_NAMES [index],
-    //         price: PRICES [index],
-    //         id_product: IDS [index],
-    //         img: IMGS [index]
-    //     }
-    // }
-
+    },
+    _handleData () {
+        for (let i = 0; i < IDS.length; i++) {
+            this.items.push (this._createNewProduct (i))
+        }
+    },
+    _createNewProduct (index) {
+        return {
+            product_name: PRODUCTS_NAMES [index],
+            price: PRICES [index],
+            id_product: IDS [index],
+            img: IMGS [index]
+        }
+    },
     render () {
         let str = ''
         this.items.forEach (item => {
             str += `
                 <div class="product-item">
-                    <img src="${item.image_link}" alt="${item.name}">
-                    <!--img src="${item.image_link}" width="300" height="200" alt="${item.name}"-->
+                    <img src="${item.img}" alt="${item.product_name}">
+                    <!--img src="${item.img}" width="300" height="200" alt="${item.product_name}"-->
                     <div class="desc">
-                        <h1>${item.name}</h1>
+                        <h1>${item.product_name}</h1>
                         <p>${item.price}</p>
                         <button 
                         class="buy-btn" 
                         name="buy-btn"
-                        data-name="${item.name}"
+                        data-name="${item.product_name}"
                         data-price="${item.price}"
-                        data-id="${item.id}"
+                        data-id="${item.id_product}"
                         >Купить</button>
                     </div>
                 </div>
@@ -86,32 +69,29 @@ import exec from "./components/requests.js";
      }
  }
 
- class Cart {
-    constructor() {
-        this.items = [];
-        this.total = 0;
-        this.sum = 0;
-        this.container = '.cart-block';
-        this.quantityBlock = document.querySelector ('#quantity');
-        this.priceBlock = document.querySelector ('#price');
-        this._init ();
-    }
-    
+ let cart = {
+    items: [],
+    total: 0,
+    sum: 0,
+    container: '.cart-block',
+    quantityBlock: document.querySelector ('#quantity'),
+    priceBlock: document.querySelector ('#price'),
+    construct () {
+        this._init ()
+    },
     _init () {
         this._handleEvents ()
-    }
-
+    },
     _handleEvents () {
         document.querySelector (this.container).addEventListener ('click', (evt) => {
             if (evt.target.name === 'del-btn') {
                 this.deleteProduct (evt.target)
             }
         })
-    }
-
+    },
     addProduct (product) {
         let id = product.dataset['id']
-        let find = this.items.find (product => product.id === id)
+        let find = this.items.find (product => product.id_product === id)
         if (find) {
             find.quantity++
         } else {
@@ -121,8 +101,7 @@ import exec from "./components/requests.js";
          
         this._checkTotalAndSum ()
         this.render ()
-    }
-
+    },
     _createNewProduct (prod) {
         return {
             product_name: prod.dataset['name'],
@@ -130,8 +109,7 @@ import exec from "./components/requests.js";
             id_product: prod.dataset['id'],
             quantity: 1
         }
-    }
-
+    },
     deleteProduct (product) {
         let id = product.dataset['id']
         let find = this.items.find (product => product.id_product === id)
@@ -143,7 +121,7 @@ import exec from "./components/requests.js";
          
         this._checkTotalAndSum ()
         this.render ()
-    }
+    },
     
     _checkTotalAndSum () {
         let qua = 0
@@ -154,8 +132,7 @@ import exec from "./components/requests.js";
         })
         this.total = qua
         this.sum = pr
-    }
-
+    },
     render () {
         let itemsBlock = document.querySelector (this.container).querySelector ('.cart-items')
         let str = ''
@@ -179,8 +156,7 @@ import exec from "./components/requests.js";
  }
  
 export default function app() {
-    let cart = new Cart()  
-    let catalog = new Catalog(cart)//тут происходит создание объекта и вся прочая магия
- 
+    catalog.construct (cart) //тут происходит создание объекта и вся прочая магия
+    cart.construct ()    
  }
 
