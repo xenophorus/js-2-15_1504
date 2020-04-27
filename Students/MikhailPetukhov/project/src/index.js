da9873f4ed978b498deaa23da1fc4f3c4d4f8eb1
unknown <petmik@yandex.ru>
2020-04-27 20:32:38 +0300

    new version of project done

---
+++ Students/MikhailPetukhov/project/src/index.js
@@ -0,0 +1,9 @@
+import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './layout/styles/normalize.css';
+import './layout/styles/style.css';
+import './layout/styles/my_style.css';
+import app from './script/js/main.js';
+
+app()
