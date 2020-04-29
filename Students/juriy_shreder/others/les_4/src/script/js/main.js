f3ff58236d1adbbc6a9a0f05fc6b84f0c349d6d1
Juriy Shreder <9090858496@mail.ru>
2020-04-29 22:13:01 +0500

    hw4

--- Students/juriy_shreder/others/les_4/src/script/js/main.js
+++ Students/juriy_shreder/others/les_4/src/script/js/main.js
@@ -1,25 +1,29 @@
-class Text {
+class TextCorr {
     constructor(container='.buy-btn'){
-        this.container = container
-        this.correct_text =''
+        this.container = container;
+        this.correct_text ='';
         this._init();
     }
     _init(){
       document.querySelector(this.container).addEventListener('click', event => {this._read();})
     }
     _read(){
+      
       let file = document.getElementById('file').files[0]
       let reader = new FileReader();
       reader.readAsText(file)
       reader.onload = function () {
-      this.correct_text = reader.result.replace(/\n/g," <br>");
-      document.getElementById('in').innerHTML = this.correct_text
-      this.correct_text = this.correct_text.replace(/(?<=\s)'|'(?=\s)|'$/g, "\"")
-      document.getElementById('out').innerHTML = this.correct_text      
-      };   
+      
+      let corr = reader.result.replace(/\n/g," <br>");
+      document.getElementById('in').innerHTML = corr
+      corr = corr.replace(/(?<=\s)'|'(?=\s)|'$/g, "\"")
+      document.getElementById('out').innerHTML = corr      
+      }; 
+      console.log(reader.onload['rezult'])
+      console.log(this) 
     }
 }
 
 export default ()=>{
-  let itm = new Text;
+  let itm = new TextCorr;
 }
