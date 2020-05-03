c75a33ea99ca37bc7de6ba0845afd5f7f137882c
kellolo <87kell@gmail.com>
2020-05-03 10:53:59 +0200

    6

---
+++ PROJ/6/src/components/containers/catalog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="products">
+    <item v-for="item of items" :item="item" :key="item.product_id" :type="'catalog'"/>
+  </div>
+</template>
+
+<script>
+import item from '../components/item.vue';
+export default {
+    components: { item },
+    data() {
+        return {
+            items: [],
+            url: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json'
+        }
+    },
+    mounted() {
+        this.$parent.get(this.url)
+        .then(data => { 
+            this.items = data; 
+        });
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
